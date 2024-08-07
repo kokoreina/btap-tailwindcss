7f4425598fc4cc7b7ee1fa949b77c0ab655d7714
TanHuy <kokoreina2005@gmail.com>
2024-08-07 17:01:17 +0700

    setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./config/style/modern-nomalize.css"
-import "./config/style/tailwind.css"
-import "./config/style/font-family.css"
-import "./config/style/main.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+//Router
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+//css
+import "./config/style/modern-nomalize.css";
+import "./config/style/tailwind.css";
+import "./config/style/font-family.css";
+import "./config/style/main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}>
+            <App />
+        </RouterProvider>
+    </React.StrictMode>,
+);
