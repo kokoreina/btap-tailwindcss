e4f382dd20028de90e905ecc3d808e287d77b446
TanHuy <kokoreina2005@gmail.com>
2024-08-07 16:37:26 +0700

    setup css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "./config/style/modern-nomalize.css"
+import "./config/style/tailwind.css"
+import "./config/style/font-family.css"
+import "./config/style/main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
